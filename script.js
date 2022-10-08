3e6b3df5dc060f1870177ac313052dff094124ac
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-10-08 01:21:20 -0400

    MVP completed

--- script.js
+++ script.js
@@ -9,30 +9,54 @@ let triviaQuestions = site.results;
 console.log(triviaQuestions);
 
 let indexCounter = 0;
-let scoreCounter = 0;
-getCurrentQuestion(indexCounter, scoreCounter)
+let numCorrectAnswer = 0;
+getCurrentQuestion(indexCounter, numCorrectAnswer)
 
 function getNextQuestion() {
   if (indexCounter < 9) {
     indexCounter += 1;
-    getCurrentQuestion(indexCounter, scoreCounter);
+    getCurrentQuestion(indexCounter, numCorrectAnswer);
+  }
+  else {
+    document.getElementById("player-score").innerHTML = numCorrectAnswer;
+    document.getElementById("display-question").innerHTML = "Congratulations! You finished the game!";
+    document.getElementById("option-one-label").innerHTML = "Correct Answers:"
+    document.getElementById("option-one").disabled = true;
+    document.getElementById("option-two").disabled = true;
+    document.getElementById("option-three").disabled = true;
+    document.getElementById("option-four").disabled = true;
+    document.getElementById("option-two-label").innerHTML = "Incorrect Answers:"
+    document.getElementById("option-three-label").innerHTML = numCorrectAnswer;
+    document.getElementById("option-four-label").innerHTML = 10 - numCorrectAnswer;
+    clearInputCheck();
   }
 }
 
-function checkAnswer() {
-  
+function clearInputCheck() {
+  document.getElementById("option-one").checked = false;
+  document.getElementById("option-two").checked = false;
+  document.getElementById("option-three").checked = false;
+  document.getElementById("option-four").checked = false;
 }
 
 function getCurrentQuestion(questionIndex, currentScore) {
   let currentTriviaQuestion = triviaQuestions[questionIndex];
+  clearInputCheck();
+
   document.getElementById("display-question").innerHTML = currentTriviaQuestion.question;
   document.getElementById("option-one-label").innerHTML = currentTriviaQuestion.correct_answer;
   document.getElementById("option-two-label").innerHTML = currentTriviaQuestion.incorrect_answers[0];
   document.getElementById("option-three-label").innerHTML = currentTriviaQuestion.incorrect_answers[1];
   document.getElementById("option-four-label").innerHTML = currentTriviaQuestion.incorrect_answers[2];
   document.getElementById("question-number").innerHTML = questionIndex + 1;
-
   document.getElementById("player-score").innerHTML = currentScore;
 }
 
+function checkCorrectAnswer() {
+  numCorrectAnswer += 1;
+  setTimeout(() => { getNextQuestion(); }, 500);
+}
 
+function checkIncorrectAnswer() {
+  setTimeout(() => { getNextQuestion(); }, 500);
+}
