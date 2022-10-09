2136d9c917cf96e210dc4b316056ff9d65d48a71
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-10-09 18:21:04 -0400

    randomized the correct answers

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ console.log(triviaQuestions);
 
 let indexCounter = 0;
 let numCorrectAnswer = 0;
+let questionArray = [];
+let correctAnswer = "";
 getCurrentQuestion(indexCounter, numCorrectAnswer)
 
 function getNextQuestion() {
@@ -20,12 +22,12 @@ function getNextQuestion() {
   else {
     document.getElementById("player-score").innerHTML = numCorrectAnswer;
     document.getElementById("display-question").innerHTML = "Congratulations! You finished the game!";
-    document.getElementById("option-one-label").innerHTML = "Correct Answers:"
+    document.getElementById("option-one-label").innerHTML = "Correct Answers:";
     document.getElementById("option-one").disabled = true;
     document.getElementById("option-two").disabled = true;
     document.getElementById("option-three").disabled = true;
     document.getElementById("option-four").disabled = true;
-    document.getElementById("option-two-label").innerHTML = "Incorrect Answers:"
+    document.getElementById("option-two-label").innerHTML = "Incorrect Answers:";
     document.getElementById("option-three-label").innerHTML = numCorrectAnswer;
     document.getElementById("option-four-label").innerHTML = 10 - numCorrectAnswer;
     clearInputCheck();
@@ -39,15 +41,64 @@ function clearInputCheck() {
   document.getElementById("option-four").checked = false;
 }
 
+function checkInput1Answer() {
+  if (questionArray[0] == correctAnswer) {
+    numCorrectAnswer += 1;
+    document.getElementById("option-one-label").className = "correct_option";
+  }
+  else {
+    document.getElementById("option-one-label").className = "option";
+  }
+  setTimeout(() => { getNextQuestion(); }, 500);
+}
+function checkInput2Answer() {
+  if (questionArray[1] == correctAnswer) {
+    numCorrectAnswer += 1;
+    document.getElementById("option-two-label").className = "correct_option";
+  }
+  else {
+    document.getElementById("option-two-label").className = "option";
+  }
+  setTimeout(() => { getNextQuestion(); }, 500);
+}
+function checkInput3Answer() {
+  if (questionArray[2] == correctAnswer) {
+    numCorrectAnswer += 1;
+    document.getElementById("option-three-label").className = "correct_option";
+  }
+  else {
+    document.getElementById("option-three-label").className = "option";
+  }
+  setTimeout(() => { getNextQuestion(); }, 500);
+}
+function checkInput4Answer() {
+  if (questionArray[3] == correctAnswer) {
+    numCorrectAnswer += 1;
+    document.getElementById("option-four-label").className = "correct_option";
+  }
+  else {
+    document.getElementById("option-four-label").className = "option";
+  }
+  setTimeout(() => { getNextQuestion(); }, 500);
+}
+
 function getCurrentQuestion(questionIndex, currentScore) {
   let currentTriviaQuestion = triviaQuestions[questionIndex];
+  correctAnswer = currentTriviaQuestion.correct_answer;
   clearInputCheck();
 
+  questionArray = [];
+  questionArray.push(currentTriviaQuestion.correct_answer);
+  questionArray.push(currentTriviaQuestion.incorrect_answers[0])
+  questionArray.push(currentTriviaQuestion.incorrect_answers[1])
+  questionArray.push(currentTriviaQuestion.incorrect_answers[2])
+  questionArray = questionArray.sort();
+
   document.getElementById("display-question").innerHTML = currentTriviaQuestion.question;
-  document.getElementById("option-one-label").innerHTML = currentTriviaQuestion.correct_answer;
-  document.getElementById("option-two-label").innerHTML = currentTriviaQuestion.incorrect_answers[0];
-  document.getElementById("option-three-label").innerHTML = currentTriviaQuestion.incorrect_answers[1];
-  document.getElementById("option-four-label").innerHTML = currentTriviaQuestion.incorrect_answers[2];
+  document.getElementById("option-one-label").innerHTML = questionArray[0];
+  document.getElementById("option-two-label").innerHTML = questionArray[1];
+  document.getElementById("option-three-label").innerHTML = questionArray[2];
+  document.getElementById("option-four-label").innerHTML = questionArray[3];
   document.getElementById("question-number").innerHTML = questionIndex + 1;
   document.getElementById("player-score").innerHTML = currentScore;
 }
